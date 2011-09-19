9850aac8ad801671da96fe4cc6b3a5e3ac037de6
Scott Smith <scott@disqus.com>
2011-09-18 20:48:47 -0700

    submoduling plugins

--- porkchop/plugins/thing.py
+++
@@ -1,8 +0,0 @@
-from porkchop.plugin import PorkchopPlugin
-
-class ThingPlugin(PorkchopPlugin):
-  def __init__(self):
-    self.refresh = 5
-
-  def get_data(self):
-    return {'fred': 'wilma', 'barney': 'betty'}
