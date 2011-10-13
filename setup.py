c001a8685261ae8dbe209e88a655fded2cc4cc17
Scott Smith <scott@disqus.com>
2011-10-13 04:33:53 -0700

    fix requires part deux

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(name='porkchop',
   packages=find_packages(),
   include_package_data=True,
   zip_safe=False,
+  install_requires=['requests'],
   entry_points={
     'console_scripts': [
       'porkchop = porkchop.commandline:main',
