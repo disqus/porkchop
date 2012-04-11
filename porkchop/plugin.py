7297e146ae382901b90abd2729e962528984e62f
Matthew Hooker <mwhooker@gmail.com>
2012-04-11 16:24:23 -0700

    Change how we load plugins.
    Allow plugins to specify metric name with magic __metric_name__ attribute.
    Don't import default plugin directory.

--- porkchop/plugin.py
+++ porkchop/plugin.py
@@ -14,7 +14,6 @@ import sys
 import socket
 import time
 
-import porkchop.plugins
 from porkchop.util import PorkchopUtil
 
 
@@ -125,7 +124,11 @@ class PorkchopPluginHandler(object):
         if directory:
             self.__class__.plugins.update(self.load_plugins(directory))
 
-        self.__class__.plugins.update(self.load_plugins(os.path.dirname(porkchop.plugins.__file__)))
+        self.__class__.plugins.update(
+            self.load_plugins(
+                os.path.join(os.path.dirname(__file__), 'plugins')
+            )
+        )
 
     def load_plugins(self, directory):
         plugins = {}
@@ -138,20 +141,34 @@ class PorkchopPluginHandler(object):
 
         for infile in glob.glob(os.path.join(directory, '*.py')):
             module_name = os.path.splitext(os.path.split(infile)[1])[0]
-            if os.path.basename(infile) != '__init__.py' and \
-                (not to_load or module_name in to_load):
-                try:
-                    module = imp.load_source(module_name, infile)
-                    for k, v in inspect.getmembers(module):
-                        if inspect.isclass(v) and issubclass(v, PorkchopPlugin):
-                            if hasattr(v, '__metric_name__'):
-                                plugin_name = v.__metric_name__
-                            else:
-                                plugin_name = k
-                            plugins[plugin_name] = v
-                            plugins[plugin_name].config_file = os.path.join(self.config_dir,
-                                '%s.ini' % module_name)
-                except ImportError:
-                    pass
+
+            if os.path.basename(infile) == '__init__.py':
+                continue
+            if to_load and module_name not in to_load:
+                continue
+
+            try:
+                module = imp.load_source(module_name, infile)
+                for namek, klass in inspect.getmembers(module):
+                    if inspect.isclass(klass) \
+                       and issubclass(klass, PorkchopPlugin) \
+                       and klass is not PorkchopPlugin:
+
+                        if hasattr(klass, '__metric_name__'):
+                            plugin_name = klass.__metric_name__
+                        else:
+                            plugin_name = module_name
+
+                        plugins[plugin_name] = klass
+                        plugins[plugin_name].config_file = os.path.join(
+                            self.config_dir,
+                            '%s.ini' % module_name
+                        )
+
+                        # Only one plugin per module.
+                        break
+
+            except ImportError:
+                pass
 
         return plugins
